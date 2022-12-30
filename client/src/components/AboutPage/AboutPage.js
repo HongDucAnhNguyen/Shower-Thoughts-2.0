55ba8fd0ac98447ccc39e5cc54a805c69e3003d5
Jay Nguyen <bobguy0606@gmail.com>
2022-12-29 19:00:49 -0800

    added temporary about page text.

--- client/src/components/AboutPage/AboutPage.js
+++ client/src/components/AboutPage/AboutPage.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div style={{padding:"100px"}}>
+      <h3> Hi 👋, I'm Jay Nguyen</h3>
+      <br />
+      <p>
+        Glad to see you here! Whether you are new or have known about this
+        project before. Welcome to Shower Thoughts! the new version (you can see the old one <a style={{cursor:"pointer", color:"orange"}} href="https://github.com/HongDucAnhNguyen/ShowerThoughts" alt="Shower Thoughts 1.0">here</a> ). A place
+        where you can share your Ingenious Contemplations that suddenly occured
+        during a Shower on a sunny Friday morning, be honest, we've all been
+        there.
+      </p>
+      <p>That's enough talking. Enjoy your stay, and remember, EUREKA!</p>
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
