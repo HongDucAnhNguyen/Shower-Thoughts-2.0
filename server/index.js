11a041db750b8d256f2bfef89d8722e7f302787b
Jay Nguyen <bobguy0606@gmail.com>
2023-02-08 16:54:58 -0800

    config

--- server/index.js
+++ server/index.js
@@ -9,18 +9,19 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 //body parser deals with the req.body
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use("/thoughts", thoughtRoutes);
-app.use("/users", userRoutes);
-//use cors as middleware for all request to restrict cross origin requests
-
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://shower-thoughts.onrender.com"],
   })
 );
-
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use("/thoughts", thoughtRoutes);
+app.use("/users", userRoutes);
+//use cors as middleware for all request to restrict cross origin requests
+app.get("/", (req, res) => {
+  res.send("running...");
+});
 //connect to database
 mongoose
   .connect(process.env.MONGO_URL, {
